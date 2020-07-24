40c896ad48b93d8bae4da90fa7a987d6eed0a74d
Shah Raiaan <smraiaan@gmail.com>
2020-07-24 18:57:25 -0400

    scenario step duplication test added

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,11 +1,32 @@
 package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class StepDefinitions {
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
         Belly belly = new Belly();
         belly.eat(cukes);
+        System.out.println("This is test output");
+        
+        
     }
+    
+    @When("I wait {int} hour")
+    public void i_wait_hour(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        
+    }
+    @Then("my belly should growl")
+    public void my_belly_should_growl() {
+        // Write code here that turns the phrase above into concrete actions
+        
+    }
+    
+    
+    
+    
+   
 }
